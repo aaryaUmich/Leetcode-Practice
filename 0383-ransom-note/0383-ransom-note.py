27488bfb9c2a9ad1d763a26feddb03a7ad8aa7cf
Aarya Upadhyay <aaryau@umich.edu>
2025-03-27 12:42:20 -0400

    Time: 31 ms (13.15%), Space: 18 MB (35.17%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,20 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
-        
-        magazineMap = {}
-        
-        
-        for c in magazine:
-            if c in magazineMap:
-                magazineMap[c] += 1
-            else:
-                magazineMap[c] = 1
+        map = {}
 
+        for c in magazine:
+            if c not in map:
+                map[c] = 1
+            else:
+                map[c]+=1
         
         for c in ransomNote:
-            if c not in magazineMap or magazineMap[c]<=0:
+            if c not in map or map[c]==0:
                 return False
-            magazineMap[c]-=1
+            else:
+                map[c]-=1
         
         return True
