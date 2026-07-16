e468d99b9ad5a37da4a6d1156425e3fcd1cc83b3
Aarya Upadhyay <aaryau@umich.edu>
2026-07-16 10:20:38 -0400

    Time: 232 ms (87.99%), Space: 21.5 MB (72.08%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(r, c):
+            if r < 0 or c < 0 or r > height-1 or c > length-1:
+                return
+            
+            if grid[r][c] == "1":
+                grid[r][c] = "0"
+                dfs(r-1,c)
+                dfs(r+1,c)
+                dfs(r,c-1)
+                dfs(r,c+1)
+        
+        length = len(grid[0])
+        height = len(grid)
+        counter = 0
+        for i in range(height):
+            for j in range(length):
+                if grid[i][j]=="1":
+                    counter+=1
+                    #do dfs
+                    dfs(i,j)
+        return counter
