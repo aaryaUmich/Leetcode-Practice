abec4eb273d3bfe54706764462518b85217fbbf9
Aarya Upadhyay <aaryau@umich.edu>
2026-07-17 13:58:25 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i:[] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visited = {}
+        def dfs(crs):
+            if crs in visited:
+                return False
+            
+            if preMap[crs] == []:
+                return True
+
+            
+            visited[crs] = 1
+            for pre in preMap[crs]:
+                if dfs(pre) == False:
+                    return False
+
+            del visited[crs]
+            preMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
+        
