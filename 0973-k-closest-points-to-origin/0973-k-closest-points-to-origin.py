ec9a925f03e01f51d62ccc7a48f7f3dc45b9a601
Aarya Upadhyay <aaryau@umich.edu>
2026-09-01 23:15:35 -0400

    Time: 83 ms (20.15%), Space: 25.1 MB (40.19%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,10 +1,19 @@
+import math
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Store (squared_distance, point) tuples
-        distances = [(p[0]**2 + p[1]**2, p) for p in points]
-        
-        # Sort by squared distance ascending
+        origin = [0,0]
+
+        distances = []
+        res = []
+
+        for point in points:
+            # Fixed: math.sqrt(...) and wrapped the pair in standard list brackets [...]
+            distances.append([math.sqrt(((origin[0]-point[0])**2) + (origin[1]-point[1])**2), point])
+
         distances.sort()
+
+        for i in range(k):
+            res.append(distances[i][1])
         
-        # Return the original point coordinates for the top k elements
-        return [point for dist, point in distances[:k]]
+        return res
